211b46161245b39fc03ea7251589b6b0be3ca6d9
Cactusuki <vikyraj6969@gmail.com>
2026-03-27 10:28:33 +0530

    Update

---
+++ Homework/Turnbased.java
@@ -0,0 +1,28 @@
+package homework;
+
+public class Turnbased {
+    public static int[] turn(int[] arr, int n) {
+        int scoreA = 0;
+        int scoreB = 0;
+        int i = 0; 
+        int j = n - 1;
+        boolean isTurnA = true;
+        while (i <= j) {
+            if (isTurnA) {
+                scoreA += arr[i];
+                i++;
+            } else {
+                scoreB += arr[j];
+                j--;
+            }
+            isTurnA = !isTurnA;
+        }
+        return new int[]{scoreA, scoreB};
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 5, 6};
+        int n = arr.length;
+        int[] results = turn(arr, n);
+        System.out.println("Final Scores -> A: " + results[0] + ", B: " + results[1]);
+    }
+}
--- array/Lec18.java
+++
@@ -1,9 +0,0 @@
-// 27th February 2026
-package array;
-
-public class Lec18 {
-    // surprise test
-    public static void main(String[] args) {
-
-    }
-}
